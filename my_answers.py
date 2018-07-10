cbf6f8a2544512df9a05c516ee2dd261595c2670
Vikram Karnaker <vkarnaker@gmail.com>
2018-07-10 20:49:22 +0000

    feat: Complete TODO #1: Implement a function to window time series

--- my_answers.py
+++ my_answers.py
@@ -12,6 +12,10 @@ def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
+    
+    for i in range(len(series)-window_size):
+        X.append(series[i:i+window_size])
+        y.append(series[i+window_size])
 
     # reshape each 
     X = np.asarray(X)
